b5ae302eab81b98b221f848014d2c6650b18f2a9
shiraez <shiraezra11@gmail.com>
2019-05-07 16:03:35 +0300

    init db

--- backend.py
+++ backend.py
@@ -11,7 +11,7 @@ import csv
 def create_table():
 
     # try:
-        with sqlite3.connect('lite.db') as conn:
+        with connect() as conn:
             cur = conn.cursor()
             cur.execute("CREATE TABLE movies (movieId TEXT PRIMARY KEY,title TEXT,genres TEXT)")
             conn.commit()
@@ -20,20 +20,37 @@ def create_table():
     # except Exception:
         # print
 
+def connect():
+    conn = sqlite3.connect('lite.db')
+    conn.text_factory = str
+    return conn
+
 
 def insert_to_db(movieId, title, genres):
-    with sqlite3.connect('lite.db') as conn:
+    with connect() as conn:
         cur = conn.cursor()
+        if('|' in genres):
+            genres = genres.split("|")[0]
         cur.execute("INSERT INTO movies VALUES (?,?,?);", (movieId, title, genres))
         conn.commit()
 
 
 def insertall():
-    with open('movies.csv', 'r') as csv_file:
+    with open('movies.csv', 'r', encoding="utf8") as csv_file:
         allmovies = csv.reader(csv_file, delimiter=',')
         for row in allmovies:
             insert_to_db(row[0], row[1], row[2])
-            print("!!!!!")
+
+
+
+def view():
+    with connect() as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM store;")
+        rows = cur.fetchall()
+        return rows
+
+
 
 
 create_table()
