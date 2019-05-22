785e45ebabba4c0d178104cd82a69d293a741358
Shira Ezra <shiraezra11@gmail.com>
2019-05-22 07:10:09 +0300

    start lab2

---
+++ Lab2/Lab2.py
@@ -0,0 +1,108 @@
+import nltk
+import random
+from nltk.corpus import movie_reviews
+from nltk.classify import ClassifierI
+from statistics import mode
+import pickle
+
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.naive_bayes import MultinomialNB,BernoulliNB
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+
+documents = [(list(movie_reviews.words(fileid)), category)
+             for category in movie_reviews.categories()
+             for fileid in movie_reviews.fileids(category)]
+
+random.shuffle(documents)
+
+all_words = []
+
+for w in movie_reviews.words():
+    all_words.append(w.lower())
+
+all_words = nltk.FreqDist(all_words)
+
+word_features = list(all_words.keys())[:5000]
+
+def find_features(document):
+    words = set(document)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)
+
+    return features
+
+
+print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
+
+testing_set = featuresets[1900:]
+training_set = featuresets[:1900]
+
+# classifier = nltk.NaiveBayesClassifier.train(training_set)
+classifier_f = open("naivebayes.pickle", "rb")
+classifier = pickle.load(classifier_f)
+classifier_f.close()
+
+
+
+print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
+
+print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
+
+# classifier.show_most_informative_features(15)
+# save_classifier = open("naivebayes.pickle","wb")
+# pickle.dump(classifier, save_classifier)
+# save_classifier.close()
+
+MNB_classifier = SklearnClassifier(MultinomialNB())
+MNB_classifier.train(training_set)
+print("MultinomialNB accuracy percent:",nltk.classify.accuracy(MNB_classifier, testing_set))
+
+BNB_classifier = SklearnClassifier(BernoulliNB())
+BNB_classifier.train(training_set)
+print("BernoulliNB accuracy percent:",nltk.classify.accuracy(BNB_classifier, testing_set))
+
+print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
+classifier.show_most_informative_features(15)
+
+MNB_classifier = SklearnClassifier(MultinomialNB())
+MNB_classifier.train(training_set)
+print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
+
+
+LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+LogisticRegression_classifier.train(training_set)
+print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+
+
+SVC_classifier = SklearnClassifier(SVC())
+SVC_classifier.train(training_set)
+print("SVC_classifier accuracy percent:", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
+
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(training_set)
+print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+
+NuSVC_classifier = SklearnClassifier(NuSVC())
+NuSVC_classifier.train(training_set)
+print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
+
+voted_classifier = VoteClassifier(
+                                  classifier,
+                                  LinearSVC_classifier,
+                                  MNB_classifier,
+                                  BernoulliNB_classifier,
+                                  LogisticRegression_classifier)
+
+
+
+
+def sentiment(text):
+    feats = find_features(text)
+    return voted_classifier.classify(feats),voted_classifier.confidence(feats)
+
+print(sentiment("This movie was awesome! The acting was great, plot was wonderful, and there were pythons...so yea!"))
+print(sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie, 0/10"))
